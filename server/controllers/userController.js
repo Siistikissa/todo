92af2790b1e1f688aa53f6595ea3e678c8c1fae2
veikka halonen <halonenveikka@gmail.com>
2024-11-11 16:19:56 +0200

    update userRouter

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,9 @@
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
 import { insertUser, selectUserByEmail } from '../models/User.js'
 import { ApiError } from '../helpers/ApiError.js'
+import jwt from 'jsonwebtoken'
+
+const { sign } = jwt
 
 const postRegistration = async(req,res, next) => {
     try {
@@ -19,7 +22,7 @@ const createUserObject = (id, email, token=undefined) => {
     return {
         'id':id,
         'email':email,
-        ...(token !== undefined && {'token':token})
+        ...(token !== undefined) && {'token':token}
     }
 }
 
@@ -27,10 +30,11 @@ const postLogin = async(req, res, next) => {
     const invalid_credentials_message = 'Invalid credentials.'
     try {
         const userFromDb = await selectUserByEmail(req.body.email)
-        if ( userFromDb === 0) return next(new ApiError(invalid_credentials_message))
+        if ( userFromDb.rowCount === 0) return next(new ApiError(invalid_credentials_message))
 
         const user = userFromDb.rows[0]
-        if (!await compare(req.body.password, user.password)) return next(new ApiError(invalid_credentials_message))
+        if (!await compare(req.body.password, user.password)) return next(new ApiError(invalid_credentials_message,401))
+
         const token = sign(req.body.email, process.env.JWT_SECRET_KEY)
         return res.status(200).json(createUserObject(user.id, user.email, token))
     } catch(error) {
--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 import { pool } from '../helpers/db.js'
 
 const insertUser = async (email, hashedPassword) => {
-    return await pool.query('insert into account (email, password) values ($1,$2) returnign *',[email,hashedPassword])
+    return await pool.query('insert into account (email, password) values ($1,$2) returning *',[email,hashedPassword])
 }
 
 const selectUserByEmail = async (email) => {
